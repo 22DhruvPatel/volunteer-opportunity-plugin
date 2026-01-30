25dbe8c037564c26ac5bf95fb6629cb457bf149d
Dhruv <dhruv.p7115@gmail.com>
2026-01-30 00:51:46 -0500

    Deleted aria_log files to resolve XAMPP issue. Created query for Volunteer_ops table inside volunteer_activate function

--- volunteer-opportunity-plugin.php
+++ volunteer-opportunity-plugin.php
@@ -7,7 +7,28 @@ Author: Dhruv Patel
 */
 
 
+function volunteer_activate() {
+  global $wpdb;
+
+  $wpdb->query("CREATE TABLE Volunteer_ops (
+    id midiumint(9) NOT NULL AUTO_INCREMENT,
+    position tinytext NOT NULL,
+    organization tinytext NOT NULL,
+    type tinytext NOT NULL,
+    email tinytext NOT NULL,
+    description text NOT NULL,
+    location tinytext NOT NULL,
+    hours int(11) NOT NULL,
+    skills text NOT NULL,
+    PRIMARY KEY (id)
+  );");
+}
 
 register_activation_hook(__FILE__, 'volunteer_activate');
+
+
+
+
+
 
 ?>
