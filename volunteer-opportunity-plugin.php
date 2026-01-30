683efcbb8b7b53e89668f10600920f3cb60885a3
Dhruv <dhruv.p7115@gmail.com>
2026-01-30 05:03:19 -0500

    basic save logic form to implement admin panel

--- volunteer-opportunity-plugin.php
+++ volunteer-opportunity-plugin.php
@@ -41,8 +41,38 @@ function volunteer_admin_menu() {
     'manage_options',
     'volunteer_ops',
     'volunteer_ops_page_html'
-  )
+  );
 }
-add_action('admin_menu', 'vlunteer_admin_menu');
+add_action('admin_menu', 'volunteer_admin_menu');
+
+function volunteer_ops_page_html() {
+  global $wpdb;
+
+  if(isset($_POST['submit'])) {
+    $table_name = 'wp_volunteer';
+
+    $wpdb->insert(
+      $table_name,
+      array(
+        'position' => $_POST['position'],
+        'hours' => $_POST['hours']
+      )
+    );
+    echo '<div class="updated"><p>Saved!</p></div/>';
+  }
+
+  ?>
+  <div class="wrap">
+    <h1>Volunteer Opportunities</h1>
+    <form method="post">
+      <label>Position Name:</label><input type="text" name="position"><br>
+      <label>Hours:</label><input type="text" name="hours"><br>
+      <input type="submit" name="submit" value="Save" class="button button-primary">
+    </form>
+  </div>
+  <?php>
+}
+ 
+
 
 ?>
